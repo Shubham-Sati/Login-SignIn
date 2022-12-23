dd227ec155eca560e3a234a51e93b8f20de10db0
shubhamsati <shubham.sati19b@gmail.com>
2022-12-23 17:14:35 +0530

    LogIn SignIn Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import SignIn from "./pages/SignIn";
+import { useState } from "react";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import { darkTheme, lightTheme } from "./utils/Theme";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.bg};
+`;
+
+const Container = styled.div`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid ${({ theme }) => theme.text};
+  border-radius: 4px;
+  position: absolute;
+  top: 3rem;
+  left: 3rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const SignInBox = styled.div`
+  width: 20rem;
+  display: flex;
+`;
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Wrapper>
+        <Container onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? (
+            <LightModeIcon style={{ color: "white" }} />
+          ) : (
+            <DarkModeIcon />
+          )}
+        </Container>
+        <SignInBox>
+          <SignIn />
+        </SignInBox>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 50px;
+  line-height: 1em;
+  border: 1px solid black;
+  color: ${({ theme }) => theme.text};
+  background-color: ${({ theme }) => theme.bgLighter};
+  border: 1px solid ${({ theme }) => theme.soft};
+`;
+const Title = styled.h1`
+  font-size: 24px;
+`;
+const Subtitle = styled.h3`
+  font-size: 20px;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  border: 1px solid ${({ theme }) => theme.soft};
+  border-radius: 3px;
+  padding: 10px;
+  background-color: transparent;
+  width: 100%;
+  color: ${({ theme }) => theme.text};
+`;
+const Button = styled.button`
+  border-radius: 3px;
+  border: none;
+  padding: 10px 20px;
+  font-weight: 500;
+  cursor: pointer;
+  background-color: ${({ theme }) => theme.soft};
+  color: ${({ theme }) => theme.textSoft};
+  margin-top: 1rem;
+`;
+
+const Signin = () => {
+  return (
+    <Wrapper>
+      <Title>Sign in</Title>
+      <Subtitle>to continue to Site</Subtitle>
+      <Input placeholder="username" />
+      <Input placeholder="password" />
+      <Button>Sign in</Button>
+      <Title>or</Title>
+      <Button>signin with Google</Button>
+      <Title>or</Title>
+      <Input placeholder="username" />
+      <Input placeholder="email" />
+      <Input placeholder="password" />
+      <Button>Sign up</Button>
+    </Wrapper>
+  );
+};
+
+export default Signin;
